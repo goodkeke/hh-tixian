2c8137ad3194bc3046ece50a213ef39d8de835e8
谈吉峰 <jerry891223@163.com>
2020-01-14 09:48:18 +0800

    更改默认请求方式

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import request from '../utils/request'
 import api from './config'
 
-export function commonApi(data, method, requestType = 'get') {
+export function commonApi(data, method, requestType = 'post') {
     let link = api.prefix + api[method] + api.version;
     let config = {
         url: link,
